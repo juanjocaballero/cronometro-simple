a85e01eb4b55754c07eef8496272b3e102174ed9
juanjocaballero <134315146+juanjocaballero@users.noreply.github.com>
2023-05-22 19:18:10 +0200

    A+B resetea el contador. Además, añade una barra giratoria mientras está contando el tiempo.

--- main.ts
+++ main.ts
@@ -1,12 +1,54 @@
-let start = 0
-let elapsed = 0
 input.onButtonPressed(Button.A, function () {
     start = input.runningTime()
 })
+input.onButtonPressed(Button.AB, function () {
+    start = 0
+})
 input.onButtonPressed(Button.B, function () {
-    elapsed = input.runningTime() - start
+    if (start == 0) {
+        elapsed = 0
+    } else {
+        elapsed = input.runningTime() - start
+    }
     basic.showNumber(Math.trunc(elapsed / 1000))
 })
+let elapsed = 0
+let start = 0
+start = 0
 basic.forever(function () {
-	
+    while (start != 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . # . .
+            . # . . .
+            # . . . .
+            `)
+        basic.pause(100)
+    }
+    basic.showNumber(start)
 })
